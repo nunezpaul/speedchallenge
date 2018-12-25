c686f7c7b5e982ea86c8812318194c4f7e2cd9a6
nunezpaul <nunez.paul@utexas.edu>
2018-12-24 22:25:32 -0800

    bucketing the speeds into discrete categories

--- create_data_pairs.py
+++ create_data_pairs.py
@@ -1,28 +1,41 @@
 import csv
 import os
 
+import pandas as pd
+
 from random import shuffle
 
 
-def write_image_value_pairs(file):
-    labels = file.format(extension='txt')
+def create_speed_category_file(file, bucket_size=3):
+    data = pd.read_csv(file, header=None)
+    data.columns = ['Speed']
+    data //= bucket_size
+    data = data.astype(int)
 
-    # read in speeds from train.txt
-    with open(labels) as f:
+    num_cats = data.max().values[0] + 1
+    file_out = file.replace('train', f'train_cat_{num_cats}')
+    data.to_csv(file_out, index=False)
+    return file_out, num_cats
+
+
+def write_image_value_pairs(file, look_back=2):
+    # read in speeds from train_cat.txt
+    with open(file) as f:
         speeds = f.read().splitlines()
 
     image_label_file = 'data/image_value_pairs.csv'
     with open(image_label_file, 'w') as f:
-        fieldnames = ['prev_image_path', 'curr_image_path', 'speed']
+        fieldnames = list(range(look_back)) + ['speed']
         writer = csv.DictWriter(f, fieldnames=fieldnames)
 
         for idx, speed in enumerate(speeds):
-            if idx <= 3:
+            if idx < (look_back - 1):
                 continue
 
-            curr_image_path = 'data/train_images/img{idx}.jpg'.format(idx=idx)
-            prev_image_path = 'data/train_images/img{idx}.jpg'.format(idx=idx - 4)
-            writer.writerow({'prev_image_path': prev_image_path, 'curr_image_path': curr_image_path, 'speed': speed})
+            write_dict = {'speed': speed}
+            for i in range(look_back):
+                write_dict[i] = 'data/train_images/img{idx}.jpg'.format(idx=idx - i)
+            writer.writerow(write_dict)
 
     print('done!')
     return image_label_file
@@ -56,26 +69,25 @@ def train_test_split(filename, split=10):
     return datafilenames['train']
 
 
-
-def shard_data(filename, num_shards=10):
+def shard_data(filename, num_cats, num_shards=10, records_per_category=200):
     datafilenames = {}
     datafiles = {}
+
+    # Split the data by its respective categorical designation
+    data = pd.read_csv(filename, header=None)
+    cat_idx = data.shape[-1] - 1
+    data_splits = []
+    for i in range(num_cats):
+        data_splits.append(data[data[cat_idx] == i])
 
     # Create filenames and open shard files to be written
     for shard in range(num_shards):
         datafilenames[shard] = 'data/sharded_image_value_pairs/image_value_pairs_{shard}.csv'.format(shard=shard)
-        datafiles[shard] = open(datafilenames[shard], 'w')
-
-    # Read each line after the header of training.csv
-    with open(filename, 'r') as f:
-        data = f.read().splitlines()
-        shuffle(data)
-
-    num_data_per_shard = int(len(data)/num_shards + 1)
-
-    for idx, line in enumerate(data):
-        which_shard = int(idx / num_data_per_shard)
-        datafiles[which_shard].write(line + '\n')
+        with open(datafilenames[shard], 'w') as f:
+            for data_split in data_splits:
+                need_replacement = data_split.shape[0] < records_per_category
+                sample = data_split.sample(records_per_category, replace=need_replacement)
+                sample.to_csv(f, header=False, index=False)
 
     return datafilenames
 
@@ -93,10 +105,13 @@ def shuffle_sharded_data(filenames):
 
 
 if __name__ == '__main__':
-    file = 'data/train.{extension}'
-    image_label_file = write_image_value_pairs(file)
+    file = 'data/train.txt'
+    cat_file, num_cats = create_speed_category_file(file)
+    image_label_file = write_image_value_pairs(cat_file)
     train_image_label_file = train_test_split(image_label_file)
-    shard_file_names = shard_data(train_image_label_file)
+    shard_file_names = shard_data(train_image_label_file, num_cats)
     shuffle_sharded_data(shard_file_names)
+
+    # remove the intermediate files 
     os.remove('data/image_value_pairs.csv')
     os.remove('data/train_image_value_pairs.csv')
