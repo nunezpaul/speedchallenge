e7bfb4548dc3fc1e987f1379573633e9d6a3484f
nunezpaul <nunez.paul@utexas.edu>
2018-12-31 13:36:46 -0800

    creating parser class to handle settings

---
+++ settings.py
@@ -0,0 +1,24 @@
+import argparse
+
+
+class Config(object):
+    def __init__(self):
+        parser = argparse.ArgumentParser(description='Parameters for training model.')
+        parser.add_argument('--tpu', action="store_true", default=False,
+                            help='determine if to be trained on tpu')
+        parser.add_argument('--opt', choices=['adam', 'sgd'], default='adam',
+                            help='which optimizer to use')
+        parser.add_argument('--lr', type=float, default=1e-3,
+                            help='set the learning rate')
+        parser.add_argument('--load_model', type=str, default=None,
+                            help='file path to saved keras model to load and continue training.')
+        parser.add_argument
+        self.settings = vars(parser.parse_args())
+
+
+if __name__ == '__main__':
+    print('here')
+    config = Config()
+    print(config)
+    for key, val in config.settings.items():
+        print(key, val)
