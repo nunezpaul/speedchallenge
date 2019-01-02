e90d773aea95617e2e7de1aa462c429d8eb19d54
nunezpaul <nunez.paul@utexas.edu>
2019-01-01 22:30:59 -0800

    added cmd line support

--- config.py
+++ config.py
@@ -2,6 +2,9 @@ import argparse
 
 
 class Config(object):
+    def __call__(self, *args, **kwargs):
+        return self.settings
+
     def __init__(self):
         parser = argparse.ArgumentParser(description='Parameters for training model.')
         parser.add_argument('--tpu', action="store_true", default=False,
@@ -12,8 +15,11 @@ class Config(object):
                             help='set the learning rate')
         parser.add_argument('--load_model', type=str, default=None,
                             help='file path to saved keras model to load and continue training.')
-        parser.add_argument
-        self.settings = vars(parser.parse_args())
+        parser.add_argument('--bucket_size', type=int, default=3,
+                            help='Set the bucket sizing that the data will be categorized into.')
+        parser.add_argument('--dropout', type=float, default=0.5,
+                            help='Dropout percent to keep during training.')
+        self.params = vars(parser.parse_args())
 
 
 if __name__ == '__main__':
