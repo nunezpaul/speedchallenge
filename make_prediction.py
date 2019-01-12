49296ff38ac13a77fb18ad1352d9b1d9c27fe308
nunezpaul <nunez.paul@utexas.edu>
2019-01-12 13:58:54 -0800

    refactoring

--- make_prediction.py
+++ make_prediction.py
@@ -9,7 +9,12 @@ if __name__ == '__main__':
         from google.colab import drive
         drive.mount('gdrive')
 
-    train_data = TrainData('data/tfrecords/train/train.tfrecord', num_shards=10, batch_size=32, len=2000)
+    train_data = TrainData('data/tfrecords/train/train.tfrecord',
+                           num_shards=1,
+                           batch_size=32,
+                           len=18360,
+                           training=True,
+                           class_weights_csv='data/labeled_csv/train/train_class_weights.csv')
     valid_data = ValidData('data/tfrecords/val/sorted_train.tfrecord', batch_size=32, len=20400)
 
     deep_vo = DeepVO(train_data=train_data, **config.params)
