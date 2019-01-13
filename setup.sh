44d3590e034d6fe8bb9988392a9f9ae8da9422e2
nunezpaul <nunez.paul@utexas.edu>
2019-01-13 03:54:26 -0800

    removing commented portion of code

--- setup.sh
+++ setup.sh
@@ -51,9 +51,9 @@ for NAME in test train val; do
 done
 
 # Convert the videos to jpegs using ffmpeg
-#ffmpeg -v || apt install ffmpeg
-#ffmpeg -i data/videos/train.mp4 -start_number 0 -qscale:v 2 data/images/train/img%d.jpg -hide_banner; echo train images done!
-#ffmpeg -i data/videos/test.mp4 -start_number 0 -qscale:v 2 data/images/test/img%d.jpg -hide_banner; echo test images done!
+ffmpeg -v || apt install ffmpeg
+ffmpeg -i data/videos/train.mp4 -start_number 0 -qscale:v 2 data/images/train/img%d.jpg -hide_banner; echo train images done!
+ffmpeg -i data/videos/test.mp4 -start_number 0 -qscale:v 2 data/images/test/img%d.jpg -hide_banner; echo test images done!
 
 # Create all img label and place in respective directory
 python create_data_pairs.py --speed_file data/train.txt --output_file data/labeled_csv/train/train.csv \
@@ -64,13 +64,13 @@ python create_data_pairs.py --speed_file data/train.txt --output_file data/label
 python create_data_pairs.py --img_dir data/images/test/ --output_file data/labeled_csv/test/test.csv --unlabeled
 
 # Writing the train, test, val and sorted val tfrecords
-#python convert_images_to_tfrecord.py --input_filename data/labeled_csv/train/train.csv \
-#--output_filename data/tfrecords/train/train.tfrecord
-#python convert_images_to_tfrecord.py --input_filename data/labeled_csv/val/val.csv \
-#--output data/tfrecords/val/val.tfrecord
-#python convert_images_to_tfrecord.py --input_filename data/labeled_csv/val/sorted_train.csv \
-#--output data/tfrecords/val/sorted_train.tfrecord
-#python convert_images_to_tfrecord.py --input_filename data/labeled_csv/test/test.csv \
-#--output data/tfrecords/test/test.tfrecord
+python convert_images_to_tfrecord.py --input_filename data/labeled_csv/train/train.csv \
+--output_filename data/tfrecords/train/train.tfrecord
+python convert_images_to_tfrecord.py --input_filename data/labeled_csv/val/val.csv \
+--output data/tfrecords/val/val.tfrecord
+python convert_images_to_tfrecord.py --input_filename data/labeled_csv/val/sorted_train.csv \
+--output data/tfrecords/val/sorted_train.tfrecord
+python convert_images_to_tfrecord.py --input_filename data/labeled_csv/test/test.csv \
+--output data/tfrecords/test/test.tfrecord
 
 echo 'Set up complete. Model is ready for training!'
