6213c46b3f6ea676749aeb3ce6ea6c38397e817b
nunezpaul <nunez.paul@utexas.edu>
2019-01-15 16:25:32 -0800

    fixed to work with refactoring

--- examine_data/examine_tfrecords.py
+++ examine_data/examine_tfrecords.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import cv2
 
-from datasets import TrainData, TestData, ValidData
+from speedchallenge.datasets import TrainData, TestData, ValidData
 
 tf.enable_eager_execution()
 
@@ -12,7 +12,7 @@ def inspect_images(data):
     for idx, img in enumerate(images):
         img_0 = img[:, :, :3]
         img_1 = img[:, :, 3:]
-        stacked_img = tf.concat([img_0, img_1], axis=0)
+        stacked_img = tf.concat([0.5 * img_0 + 0.5 * img_1,  img_0, img_1], axis=0)
 
         print(f'image {idx}')
         cv2.imshow('image', stacked_img.numpy()[..., ::-1])
@@ -22,10 +22,14 @@ def inspect_images(data):
 
 if __name__ == '__main__':
     tf.enable_eager_execution()
-    train_data = TrainData('data/tfrecords/train/train.tfrecord', num_shards=1, batch_size=32, len=18385, training=True)
-    valid_data = ValidData('data/tfrecords/val/val.tfrecord', batch_size=32, len=2130)
-    sorted_train_data = ValidData('data/tfrecords/val/sorted_train.tfrecord', batch_size=32, len=20400)
-    test_data = TestData('data/tfrecords/test/test.tfrecord', batch_size=32, len=10797)
+    train_data = TrainData('../data/tfrecords/train/train.tfrecord',
+                           num_shards=1,
+                           batch_size=32,
+                           len=18385,
+                           training=True)
+    valid_data = TestData('../data/tfrecords/val/val.tfrecord', batch_size=32, len=2130)
+    sorted_train_data = TestData('../data/tfrecords/val/sorted_train.tfrecord', batch_size=64, len=20400)
+    test_data = TestData('../data/tfrecords/test/test.tfrecord', batch_size=64, len=10797)
 
-    for idx, data in enumerate((train_data, valid_data, sorted_train_data, test_data)):
+    for idx, data in enumerate(( sorted_train_data, test_data)):
         inspect_images(data)
